4cd732e3bee04ea49d600379359d4f012800f678
Erik J. Sturcke <esturcke@cisco.com>
2015-07-08 07:27:38 -0400

    Model song

---
+++ Song.elm
@@ -0,0 +1,28 @@
+module Song where
+
+-- MODEL
+
+type alias Song = {
+    title   : String,
+    artist  : String,
+    stanzas : List Stanza
+}
+
+type StanzaKind = Verse | Prechorus | Chorus | Bridge
+
+type alias Stanza = {
+    kind  : StanzaKind,
+    lines : List Phrase
+}
+
+type alias Phrase = {
+    chord : Chord,
+    lyric : String
+}
+
+type alias Chord = {
+    label : String,
+    notes : List Int
+}
+
+
